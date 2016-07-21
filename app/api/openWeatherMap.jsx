0e0e7284f6ecc35c107b1535fe260d596e55b595
Jorge <jorge7139@gmail.com>
2016-07-21 10:59:46 -0500

    Metric units

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=36b0153db40080637eca18ba20b86dc2&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=36b0153db40080637eca18ba20b86dc2&units=metric';
 
 module.exports = {
     getTemp: function(location) {
