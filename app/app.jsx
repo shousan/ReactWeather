808d8063d0150476eb96a39d64ffef466319f1ee
Jorge <jorge7139@gmail.com>
2016-07-21 11:18:49 -0500

    Add CSS styles

--- app/app.jsx
+++ app/app.jsx
@@ -10,13 +10,14 @@ var Examples = require('Examples');
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
+// App css
+require('style!css!applicationStyles')
+
 ReactDOM.render(
-  <Router history={hashHistory}>
+    <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="about" component={About}/>
-      <Route path="examples" component={Examples}/>
-      <IndexRoute component={Weather}/>
+        <Route path="about" component={About}/>
+        <Route path="examples" component={Examples}/>
+        <IndexRoute component={Weather}/>
     </Route>
-  </Router>,
-  document.getElementById('app')
-);
+</Router>, document.getElementById('app'));
--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -5,60 +5,45 @@ var ErrorModal = require('ErrorModal');
 var openWeatherMap = require('openWeatherMap');
 
 var Weather = React.createClass({
-  getInitialState: function () {
-    return {
-      isLoading: false
-    }
-  },
-  handleSearch: function (location) {
-    var that = this;
+    getInitialState: function() {
+        return {isLoading: false}
+    },
+    handleSearch: function(location) {
+        var that = this;
 
-    this.setState({
-      isLoading: true,
-      errorMessage: undefined
-    });
+        this.setState({isLoading: true, errorMessage: undefined});
 
-    openWeatherMap.getTemp(location).then(function (temp) {
-      that.setState({
-        location: location,
-        temp: temp,
-        isLoading: false
-      });
-    }, function (e) {
-      that.setState({
-        isLoading: false,
-        errorMessage: e.message
-      });
-    });
-  },
-  render: function () {
-    var {isLoading, temp, location, errorMessage} = this.state;
+        openWeatherMap.getTemp(location).then(function(temp) {
+            that.setState({location: location, temp: temp, isLoading: false});
+        }, function(e) {
+            that.setState({isLoading: false, errorMessage: e.message});
+        });
+    },
+    render: function() {
+        var {isLoading, temp, location, errorMessage} = this.state;
 
-    function renderMessage () {
-      if (isLoading) {
-        return <h3 className="text-center">Fetching weather...</h3>;
-      } else if (temp && location) {
-        return <WeatherMessage temp={temp} location={location}/>;
-      }
-    }
+        function renderMessage() {
+            if (isLoading) {
+                return <h3 className="text-center">Fetching weather...</h3>;
+            } else if (temp && location) {
+                return <WeatherMessage temp={temp} location={location}/>;
+            }
+        }
 
-    function renderError () {
-      if (typeof errorMessage === 'string') {
+        function renderError() {
+            if (typeof errorMessage === 'string') {
+                return (<ErrorModal message={errorMessage}/>)
+            }
+        }
+
         return (
-          <ErrorModal message={errorMessage}/>
+            <div>
+                <h1 className="text-center page-title">Get Weather</h1>
+                <WeatherForm onSearch={this.handleSearch}/> {renderMessage()}
+                {renderError()}
+            </div>
         )
-      }
     }
-
-    return (
-      <div>
-        <h1 className="text-center">Get Weather</h1>
-        <WeatherForm onSearch={this.handleSearch}/>
-        {renderMessage()}
-        {renderError()}
-      </div>
-    )
-  }
 });
 
 module.exports = Weather;
