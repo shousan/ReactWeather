4cfd589a24e81f597e01049eca008923557ab8a4
Jorge <jorge7139@gmail.com>
2016-07-21 10:30:58 -0500

    Changed everything

---
+++ example-arrow-function.js
@@ -0,0 +1,42 @@
+// var names = ['Jorge', 'Joan', 'Andrés'];
+// //
+// // names.forEach(function (name) {
+// //   console.log('forEach', name);
+// // });
+// //
+// // names.forEach((name) => {
+// //   console.log('arrowFunc', name);
+// // });
+// //
+// // names.forEach((name) => console.log(name));
+//
+//
+// // var returnMe = (name) => name + '!'
+// // console.log(returnMe('Jorge'));
+//
+// var person = {
+//   name: 'Jorge',
+//   greet: function () {
+//       names.forEach( (name) => {
+//         console.log(this.name + ' says hi to ' + name)
+//       })
+//   }
+// };
+//
+// person.greet();
+
+
+// Challenge Area
+function add (a, b) {
+  return a + b;
+}
+
+// addStatement
+var addStatement = (a, b) => {
+  return a + b;
+}
+console.log(addStatement(4,7));
+
+// addExpressiom
+var addExpression = (a, b) => a + b;
+console.log(addExpression(3, -1));
